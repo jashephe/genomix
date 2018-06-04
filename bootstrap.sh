aee7e7b311bbe49f74f17bf5d572e44b305a720b
James Shepherdson <james.shepherdson@wustl.edu>
2018-06-04 10:26:48 -0500

    Adding git installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,5 @@
 pacman -Syu --noconfirm
 
 # Install packages
-pacman -S --noconfirm fish base-devel neovim python python-pip openldap sssd
+pacman -S --noconfirm fish base-devel git neovim python python-pip openldap sssd
 pip install bpython virtualfish neovim numpy scipy jupyter requests
