f52ad10c54a995465c27cfe234d7285b893fb6a9
James Shepherdson <james.shepherdson@wustl.edu>
2018-06-11 14:07:57 -0500

    Adding AUR building, rust-parallel

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,3 +9,12 @@ pacman -Syu --noconfirm
 # Install packages
 pacman -S --noconfirm fish base-devel clang zlib bzip2 xz man-db gawk grep gzip less sed tar which coreutils procps-ng htop file git tmux xclip neovim python python-pip openldap sssd
 pip install bpython virtualfish neovim numpy scipy jupyter requests
+
+# Install other packages
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+
+yay -S parallel-rust
