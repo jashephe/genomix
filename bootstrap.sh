77735e5e15d64e36ee1fd34cb96b7b30ed779499
James Shepherdson <james.shepherdson@wustl.edu>
2018-06-12 12:04:12 -0500

    Reformatting, adding jre8

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,5 +7,11 @@ ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
 pacman -Syu --noconfirm
 
 # Install packages
-pacman -S --noconfirm fish base-devel clang zlib bzip2 xz man-db gawk grep gzip less sed tar which coreutils parallel procps-ng htop file git tmux xclip neovim python python-pip openldap sssd
+pacman -S --noconfirm fish man-db coreutils gawk grep less sed which procps-ng
+pacman -S --noconfirm gzip zlib bzip2 xz tar
+pacman -S --noconfirm base-devel git clang python python-pip jre8-openjdk
+pacman -S --noconfirm parallel htop tmux neovim xclip
+pacman -S --noconfirm openldap sssd
+
+# Install other packages
 pip install bpython virtualfish neovim numpy scipy jupyter requests
