da916842b22145d52110bd02eedebf121e1fb64e
James Shepherdson <james.shepherdson@wustl.edu>
2018-06-14 15:28:01 -0500

    Adding X packages, package tools

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
 
 # Initialize pacman databases
 pacman -Syu --noconfirm
+pacman -Fy --noconfirm
 
 # Install packages
 pacman -S --noconfirm fish man-db coreutils gawk grep less sed which procps-ng
@@ -12,7 +13,7 @@ pacman -S --noconfirm gzip zlib bzip2 xz zip unzip tar
 pacman -S --noconfirm base-devel git clang python python-pip jre8-openjdk gsl
 pacman -S --noconfirm parallel htop tmux neovim xclip
 pacman -S --noconfirm openldap sssd
-pacman -S --noconfirm xorg
+pacman -S --noconfirm xorg xorg-xinit
 
 # Install other packages
 pip install bpython virtualfish neovim numpy scipy jupyter requests
