92279169e03cea541efb43df4db261082e2c4f0a
James Shepherdson <james.shepherdson@wustl.edu>
2018-06-18 15:25:19 -0500

    Switching to perlbrew

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ pacman -Fy --noconfirm
 # Install packages
 pacman -S --noconfirm fish man-db coreutils gawk grep less sed which procps-ng
 pacman -S --noconfirm gzip zlib bzip2 xz zip unzip tar
-pacman -S --noconfirm base-devel git clang python python-pip pypy3 jre8-openjdk gsl perl cpanminus perl-local-lib perl-dbd-mysql
+pacman -S --noconfirm base-devel git clang python python-pip pypy3 jre8-openjdk gsl perlbrew
 pacman -S --noconfirm parallel htop tmux neovim xclip
 pacman -S --noconfirm openldap sssd
 
